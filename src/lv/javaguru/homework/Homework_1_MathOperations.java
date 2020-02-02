f88d0727a976311900e32aed911826c5f43757ae
agasiorowska <olaagasiorowska@gmail.com>
2020-02-02 13:29:51 +0200

    homework 1

---
+++ src/lv/javaguru/homework/Homework_1_MathOperations.java
@@ -0,0 +1,16 @@
+package lv.javaguru.homework;
+
+public class Homework_1_MathOperations {
+    public static void main(String[] args) {
+        int firstNumber = 10;
+        int secondNumber = 5;
+
+        int sumResult = firstNumber + secondNumber;
+        int subtractResult = firstNumber - secondNumber;
+        int divideResult = firstNumber / secondNumber;
+        int multiplyResult = firstNumber * secondNumber;
+
+        System.out.println("Sum result: " + sumResult);
+        System.out.println("Subtract result: " + subtractResult);
+        System.out.println("Divide result: " + divideResult);
+        System.out.println("Multiply result: " + multiplyResult); } }
