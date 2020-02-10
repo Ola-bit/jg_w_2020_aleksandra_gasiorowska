ca82d4fb326eb600cca23578378fbcbba3a8aa27
agasiorowska <olaagasiorowska@gmail.com>
2020-02-10 17:51:12 +0200

    RandomNumbersServiceTest

---
+++ src/lv/javaguru/homework/Circle.java
@@ -0,0 +1,29 @@
+package lv.javaguru.homework;
+
+class Circle {
+
+    double radius;
+
+    public double getRadius() {
+
+        return radius;
+
+    }
+
+
+
+    public void setRadius(double radius) {
+
+        this.radius = radius;
+
+    }
+
+
+
+    double calculateArea() {
+
+        return 3.14 * radius * radius;
+
+    }
+
+}
