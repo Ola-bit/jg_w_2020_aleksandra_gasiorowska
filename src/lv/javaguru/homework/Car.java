501129780e64059c0854f7db834e90a2fbdd35d9
agasiorowska <olaagasiorowska@gmail.com>
2020-02-10 18:52:40 +0200

    RandomNumbersServiceTest

---
+++ src/lv/javaguru/homework/Car.java
@@ -0,0 +1,33 @@
+package lv.javaguru.homework;
+
+public class Car {
+
+    String color;
+    public String getColor() {
+        return color;
+    }
+
+    String brand;
+    public String getBrand() {
+        return brand;
+    }
+
+    int year;
+    public int getYear() {
+        return year;
+    }
+
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+};
