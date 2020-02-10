e834a701e96cd8f5b851985df4b2f6782c2c4928
agasiorowska <olaagasiorowska@gmail.com>
2020-02-10 19:01:19 +0200

    RandomNumbersServiceTest

---
+++ src/lv/javaguru/homework/CarTest.java
@@ -0,0 +1,16 @@
+package lv.javaguru.homework;
+
+public class CarTest {
+
+    public static void main(String[] args) {
+        Car car = new Car();
+        car.setColor("White");
+        car.setBrand("Tesla");
+        car.setYear(2018);
+
+        System.out.println(car.color +" " + car.brand +" " + "produced in " + car.year);
+
+    }
+};
+
+
