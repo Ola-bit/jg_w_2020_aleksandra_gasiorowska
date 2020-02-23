bf16b396345c1b05452a64d33d2144813d9fe57e
agasiorowska <olaagasiorowska@gmail.com>
2020-02-23 21:00:22 +0200

    Homework_4

---
+++ src/Homework_4/BookTest.java
@@ -0,0 +1,22 @@
+package Homework_4;
+
+public class BookTest {
+
+    public static void main(String[] args) {
+
+        Book b1 = new Book ("J. K. Rowling", "Harry Potter and the Sorcerers' Stone", 289);
+        Book b2 = new Book ("J. K. Rowling", "Harry Potter and the Order of Phoenix", 589);
+        Book b3 = new Book ("Snow", "Head First Java", 841);
+        Book b4 = new Book ("J. K. Rowling" , "Harry Potter and the Sorcerers' Stone", 289);
+
+        System.out.println("The library currently holds books by " + b1.getAuthor() + " and " + b3.getAuthor() + ".");
+
+        System.out.println("is b1 same as b1? " + b1.equals(b1));
+        System.out.println("is b2 same as b1? " + b2.equals(b1));
+        System.out.println("is b3 same as b1? " + b3.equals(b1));
+        System.out.println("is b4 same as b1? " + b4.equals(b1));
+        System.out.println();
+
+
+    }
+}
